a0e4253419db2f994efa41ed4bc6bed6a3ac000e
Sergey <30868885+zloysergunya@users.noreply.github.com>
2019-09-21 15:43:51 +0700

    Update Lab4_Kotov.sql

--- Lab4_Kotov.sql
+++ Lab4_Kotov.sql
@@ -1,5 +1,24 @@
 ﻿-- Вывести количество сделок, зафиксированных в БД.
 SELECT COUNT(*) AS [Count Deal]
-	FROM Deal
+	FROM Deal;
 
--- Определить среднее арифметическое значение количества товаров на складе.SELECT AVG(Warehouse.Count) AS [AVG Count Goods]	FROM Warehouse-- Определить общее количество товаров на складе.SELECT SUM(Warehouse.Count) AS [SUM Count Goods]	FROM Warehouse-- Определить максимальное количество товаров на складе.SELECT MAX(Warehouse.Count) AS [Max Count Goods]	FROM Warehouse-- Вывести значение косинуса для угла 60ºSELECT COS(RADIANS(60.0)) AS COS-- Получить целое случайное число в диапазоне (-7; 2)SELECT ROUND((RAND()*9)-6, 0) AS RAND-- Определить (вывести в виде числа от 1 до 7) текущий день неделиSELECT DATEPART(DW, GETDATE()) [Index Day today]
+-- Определить среднее арифметическое значение количества товаров на складе.
+SELECT AVG(Warehouse.Count) AS [AVG Count Goods]
+	FROM Warehouse;
+
+-- Определить общее количество товаров на складе.
+SELECT SUM(Warehouse.Count) AS [SUM Count Goods]
+	FROM Warehouse;
+
+-- Определить максимальное количество товаров на складе.
+SELECT MAX(Warehouse.Count) AS [Max Count Goods]
+	FROM Warehouse;
+
+-- Вывести значение косинуса для угла 60º
+SELECT COS(RADIANS(60.0)) AS COS;
+
+-- Получить целое случайное число в диапазоне (-7; 2)
+SELECT ROUND((RAND()*9)-6, 0) AS RAND;
+
+-- Определить (вывести в виде числа от 1 до 7) текущий день недели
+SELECT DATEPART(DW, GETDATE()) [Index Day today];
